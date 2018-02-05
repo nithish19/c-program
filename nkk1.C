83a761aa3b8440056f6256007411560d8531f5df
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-05 15:53:44 +0530

    nkk1.C

---
+++ nkk1.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    long long a;
+    int count = 0;
+    clrscr();
+
+    printf("Enter an integer: ");
+    scanf("%lld", &a);
+
+    while(a != 0)
+    {
+       
+        a /= 10;
+        ++count;
+    }
+
+    printf("Number of digits: %d", count);
+    return 0;
+    getch();
+}
