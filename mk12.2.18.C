5797ca27144e3b9486962142a2ee7ba0dd8c6dd6
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-12 16:18:40 +0530

    Create mk12.2.18.C

---
+++ mk12.2.18.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int b[10],temp;
+  int i;
+  for(i=0;i<10;i++)
+  {
+    scanf("%d",&b[i]);
+  }
+  temp=a[0];
+  for(i=0;i<10;i++)
+  {
+    if(a[0]>b[i])
+    {
+      temp=b[i]);
+    }
+    printf("the largest number is %d",temp);
+  }
+}
