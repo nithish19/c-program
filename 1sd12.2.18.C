4203ca045542e6f98b4e35301fc18d070755b391
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-12 16:10:35 +0530

    Update and rename sd12.2.18.C to 1sd12.2.18.C

--- 1sd12.2.18.C
+++ 1sd12.2.18.C
@@ -1,18 +1,19 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
-  int a[10],temp;
+  int b[10],temp;
   int i;
   for(i=0;i<10;i++)
   {
-    scanf("%d",&a[i]);
+    scanf("%d",&b[i]);
   }
   temp=a[0];
   for(i=0;i<10;i++)
   {
-    if(a[0]<a[i])
+    if(a[0]<b[i])
     {
-      temp=a[i]);
+      temp=b[i]);
     }
     printf("the largest number is %d",temp);
   }
