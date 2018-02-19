9f18a9ae635b071eb365ce9fac5c9048183ee60f
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-19 16:16:49 +0530

    Create 2mech19.2.18.C

---
+++ 2mech19.2.18.C
@@ -0,0 +1,12 @@
+-#include<stdio.h>
+#include<conio.h>https://github.com/nithish19/c-program/subscription
+int main()
+{
+int a,b,c;
+printf("enter the value a,b);
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("enter the value %d",c);
+getch();
+return 0;
+}
