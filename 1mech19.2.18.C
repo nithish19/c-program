bd884f843ed5f8cd3393218d16c578e9872208bc
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-19 16:08:00 +0530

    Create 1mech19.2.18.C

---
+++ 1mech19.2.18.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("enter the values a");
+scanf("%d",&a);
+printf("enter the value b");
+scanf("%d",&b);
+b=a*a;
+getch();
+return 0;
+}
