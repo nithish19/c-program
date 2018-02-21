c2c3601955b9746be0e1b0edc97b2f05eaf747e6
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-21 16:10:49 +0530

    Create 3rock21.2.18.C

---
+++ 3rock21.2.18.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter the value a");
+scanf("%d",&a);
+printf("enter the value b");
+scanf("%d",&b);
+c=a+b;
+printf("enter the value %d",c);
+getch(0;
+return 0;
+}
