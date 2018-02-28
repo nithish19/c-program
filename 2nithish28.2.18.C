aa20c23a05c9be6056edf878c1a650e7d14e93a4
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-28 15:51:33 +0530

    Create 2nithish28.2.18.C

---
+++ 2nithish28.2.18.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x,y,z;
+printf("enter the value x,y);
+scanf("%d%d",&x,&y);
+z=x+y;
+printf("enter the value %d",z);
+getch();
+return 0;
+}
