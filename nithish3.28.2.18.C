ffe1952802f1dfcff1dd2950456f40ae24f74fa6
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-28 15:57:07 +0530

    Create nithish3.28.2.18.C

---
+++ nithish3.28.2.18.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float hour,minute,d;
+  printf("enter the two values"):
+  scanf("%f%f",&hour,&minute);
+  {
+    d=hour-minute;
+    printf("%d",d);
+    getch();
+    return 0;
+  }
+}
