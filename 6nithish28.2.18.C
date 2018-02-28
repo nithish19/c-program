0288f6a88929242f9e88ebc19996a9f3e580313f
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-28 16:09:25 +0530

    Create 6nithish28.2.18.C

---
+++ 6nithish28.2.18.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int b[10],temp;
+  int i;
+  for(i=0;i<10;i++)
+  {
+    scanf("%d",&b[i]);
+  }
+  temp=a[0];
+  for(i=0;i<10;i++)
+  {
+    if(a[0]>b[i])
+    {
+      temp=b[i]);
+    }
+    printf("the largest number is %d",temp);
+  }
+}
