5df2cb38d3be19c4a97f6a739450e9e42336e013
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-03-02 13:13:26 +0530

    Create b.2.3.18.C

---
+++ b.2.3.18.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char s1[100],s2[100];
+int res;
+clrscr();
+printf("Enter S1 and S2:");
+gets(s1);
+gets(s2);
+res=strcmp(s1,s2);
+if(res==0)
+printf("Strings are equal");
+else
+printf("Strings are not equal");
+getch();
+}
+int strcmp(char *s1,char *s2)
+{
+int i=0;
+while(s1[i]!='\0'||s2[i]!='\0')
+{
+if(s1[i]!=s2[i])
+return (s2[i]-s1[i]);
+i++;
+}
+getch();
+return 0;
+}
