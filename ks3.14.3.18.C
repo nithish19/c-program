7fcca486d38de107724bb7d6671a7e94bd62fa5f
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-03-14 03:26:08 -0700

    Create ks3.14.3.18.C

---
+++ ks3.14.3.18.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+   int x,y;
+   scanf("%d %d",&x,&y);
+   if(a!=0)
+   {
+       x=x^y;
+      y=y^x;
+      x=x^y;
+   }
+   printf("%d %d",x,y);
+   getch();
+    return 0;
+}
